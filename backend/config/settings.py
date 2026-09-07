cb9fb45e616b9593186f2f2bbab2759862152524
apple <apple@apples-MacBook-Air.local>
2026-09-07 16:01:18 -0700

    Normalize deployed CORS origins

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,10 +130,12 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOWED_ORIGINS = [origin for origin in os.environ.get(
+CORS_ALLOWED_ORIGINS = [origin.strip().rstrip('/') for origin in os.environ.get(
     'CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173'
-).split(',') if origin]
-CSRF_TRUSTED_ORIGINS = [origin for origin in os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if origin]
+).split(',') if origin.strip()]
+CSRF_TRUSTED_ORIGINS = [origin.strip().rstrip('/') for origin in os.environ.get(
+    'CSRF_TRUSTED_ORIGINS', ''
+).split(',') if origin.strip()]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
